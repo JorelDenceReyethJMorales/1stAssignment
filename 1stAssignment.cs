24027ceb59e09f143a960f17aa511c1063a54964
JorelDenceReyethJMorales <164605087+JorelDenceReyethJMorales@users.noreply.github.com>
2024-03-23 14:07:57 +0800

    Add files via upload
    1st Assignment ( Number 1 ) 
    O O P

---
+++ 1stAssignment.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.Design;
+
+namespace _1stAssignments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = "Admin";
+            string password = "Admin1234";
+            int Attempts = 3;
+
+            while (Attempts >= 1)
+            {
+                Console.Write("Username : ");
+                string x = Console.ReadLine();
+
+                Console.Write("Password : ");
+                string y = Console.ReadLine();
+
+                if (x == username && y == password) break;
+                else 
+                {
+                    Attempts--;
+                    Console.WriteLine("Incorrect Credentials"); 
+                    Console.WriteLine();
+                    
+                    if (Attempts >= 1)
+                    {
+                        Console.WriteLine(Attempts + " more try");
+                        Console.WriteLine();
+                    } 
+                }
+            } 
+                if (Attempts >= 1) 
+                {
+                    Console.WriteLine("Login Successfully");
+                }
+                else 
+                {
+                    Console.WriteLine("Maximum Attempt");
+                } 
+        }
+    }
+}
