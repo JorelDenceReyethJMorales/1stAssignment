982d7e1b3c97e6cc7cb6822b3cf607b738ec68c4
JorelDenceReyethJMorales <164605087+JorelDenceReyethJMorales@users.noreply.github.com>
2024-03-31 10:38:58 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace ASSIGNMENT_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = "Admin";
+            string password = "Admin1234";
+            int Attempts = 3;
+
+            while (Attempts >= 1)
+            {
+                Console.Write("Username : ");
+                string x = Console.ReadLine();
+
+                Console.Write("Password : ");
+                string y = Console.ReadLine();
+
+                if (x == username && y == password) break;
+                else
+                {
+                    Attempts--;
+                    Console.WriteLine("Incorrect Credentials");
+                    Console.WriteLine();
+
+                    if (Attempts >= 1)
+                    {
+                        Console.WriteLine(Attempts + " more try");
+                        Console.WriteLine();
+                    }
+                }
+            }
+            if (Attempts >= 1)
+            {
+                Console.WriteLine("Login Successfully");
+            }
+            else
+            {
+                Console.WriteLine("Maximum Attempt");
+            }
+        }
+    }
+}
